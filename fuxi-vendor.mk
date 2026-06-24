3bf4e9ec597325dd49df4e2420e14b94af521047
raebaexxx <vadimfeda@yandex.ru>
2026-06-24 19:47:05 +0300

    fuxi: add primitive effect files to PRODUCT_COPY_FILES
    Without these entries, the primitive_effect_*.bin files would not be
    copied to /vendor/etc/vibrator/ on the device, causing the vibrator
    HAL to fall back to hardcoded waveforms.

--- fuxi-vendor.mk
+++ fuxi-vendor.mk
@@ -701,7 +701,17 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/effect_2.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/effect_2.bin \
     vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/effect_3.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/effect_3.bin \
     vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/effect_4.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/effect_4.bin \
-    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/effect_5.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/effect_5.bin
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/effect_5.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/effect_5.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_0.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_0.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_1.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_1.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_2.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_2.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_3.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_3.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_4.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_4.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_5.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_5.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_6.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_6.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_7.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_7.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_8.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_8.bin \
+    vendor/xiaomi/fuxi/proprietary/vendor/etc/vibrator/primitive_effect_9.bin:$(TARGET_COPY_OUT_VENDOR)/etc/vibrator/primitive_effect_9.bin
 
 PRODUCT_PACKAGES += \
     libPlatformValidatorShared-camera \
